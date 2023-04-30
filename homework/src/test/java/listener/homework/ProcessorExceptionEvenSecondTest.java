8673baddacae6a02f285488a7843ce31f54216d5
levklg <myevent@mail.ru>
2023-04-30 15:26:34 +0300

    commit

--- homework/src/test/java/listener/homework/ProcessorExceptionEvenSecondTest.java
+++ homework/src/test/java/listener/homework/ProcessorExceptionEvenSecondTest.java
@@ -1,26 +1,35 @@
 package listener.homework;
 
 import model.Message;
-
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import java.util.function.Supplier;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import processor.homework.ProcessorExceptionEvenSecond;
 import java.time.LocalDateTime;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.mockito.Mockito.when;
 
 public class ProcessorExceptionEvenSecondTest {
 
-
     private ProcessorExceptionEvenSecond processor;
 
+    @Mock
+    private Supplier<LocalDateTime> currentTimeSupplier;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void processThrowsExceptionOnEvenSecond() throws InterruptedException {
+    void processThrowsExceptionOnEvenSecond() {
 
-        processor = new ProcessorExceptionEvenSecond(LocalDateTime::now);
+        when(currentTimeSupplier.get()).thenReturn(LocalDateTime.of(2022, 1, 1, 1, 1, 2));
 
-        if (LocalDateTime.now().getSecond() % 2 != 0) {
-            Thread.sleep(1000);
-        }
+        processor = new ProcessorExceptionEvenSecond(currentTimeSupplier);
 
         assertThatThrownBy(() -> processor.process(new Message.Builder(1).build()))
                 .isInstanceOf(RuntimeException.class)
@@ -28,12 +37,12 @@ public class ProcessorExceptionEvenSecondTest {
     }
 
     @Test
-    void processDoesNotThrowExceptionOnOddSecond() throws InterruptedException {
+    void processDoesNotThrowExceptionOnOddSecond() {
 
-        processor = new ProcessorExceptionEvenSecond(LocalDateTime::now);
-        if (LocalDateTime.now().getSecond() % 2 == 0) {
-            Thread.sleep(1000);
-        }
+        when(currentTimeSupplier.get()).thenReturn(LocalDateTime.of(2022, 1, 1, 1, 1, 3));
+
+        processor = new ProcessorExceptionEvenSecond(currentTimeSupplier);
+
         assertThatCode(() -> processor.process(new Message.Builder(1).build())).doesNotThrowAnyException();
     }
 }
